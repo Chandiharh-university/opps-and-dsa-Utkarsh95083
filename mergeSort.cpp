33abb11830cf685b5e8212b3444816d75081d892
Utkarsh95083 <22BCS10233@cuchd.in>
2024-12-20 23:06:24 +0530

    Add files via upload

---
+++ mergeSort.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(vector<int> &arr,int mid,int low,int high){
+    vector<int> temp;
+    int right =  mid+1;
+    int left = low;
+    while(left<=mid&&right<=high){
+        if(arr[left]<=arr[right]){
+          temp.push_back(arr[left]);
+          left++;
+        }
+        else{
+             temp.emplace_back(arr[right]);
+             right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+     while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i = low;i<=high;i++){
+        arr[i]=temp[i-low];
+    }
+}
+void mergesort(vector<int>& arr,int low , int high){
+    if(low>=high){
+        return;
+    }
+    int mid = (low + high)/2;
+    mergesort(arr,low,mid);
+    mergesort(arr,mid+1,high);
+    merge(arr,mid,low,high);
+
+
+}
+int main(){
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter elements "<<endl;
+    for(int i = 0; i< n;i++){
+        cin>>arr[i];
+    }
+    
+    
+    mergesort(arr,0,n-1);
+    cout<<"Sorted array"<<endl;
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
